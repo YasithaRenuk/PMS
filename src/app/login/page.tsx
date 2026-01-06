e388030787d914815ec5739a3c0190f10deb61e0
Yasitha Renuka <126389901+YasithaRenuk@users.noreply.github.com>
2026-01-06 20:22:24 +0530

    redrict to dashbord

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ export default async function LoginPage() {
   const session = await getServerAuthSession();
 
   if (session?.user) {
-    redirect("/");
+    redirect("/dashboard");
   }
 
   return (
